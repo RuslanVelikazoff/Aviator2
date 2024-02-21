55f11f190c5e2f536a44b5e7567c50d295c432cf
Ruslan155 <coperonplay3@gmail.com>
2024-02-21 17:12:17 +0300

    gameManager for player

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,16 +4,44 @@ public class GameManager : MonoBehaviour
 {
     public int score;
 
+    private Player player;
     private LevelUIManager levelUI;
 
     private void Start()
     {
         levelUI = FindObjectOfType<LevelUIManager>();
+        player = FindObjectOfType<Player>();
     }
 
     public void AddScore()
     {
         score++;
         levelUI.SetScoreText();
+    }
+
+    public void DamagePlayer(GameObject enemy)
+    {
+        Destroy(enemy);
+        player.Health--;
+        levelUI.SetHealthBar();
+
+        if (player.Health <= 0)
+        {
+            LoseGame();
+        }
+    }
+
+    private void LoseGame()
+    {
+        Time.timeScale = 0;
+
+        if (score > PlayerPrefs.GetInt(Constants.DATA.HIGH_SCORE))
+        {
+            //Open new record panel
+        }
+        else
+        {
+            //Open standart panel
+        }
     }
 }
